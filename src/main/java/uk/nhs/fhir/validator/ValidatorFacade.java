e3d81d86370ca31a2e8ffc3a41c8927b0a261aba
Tim Coates <tim.coates@gmail.com>
2016-10-15 09:25:25 +0100

    Added a facade class to reduce code duplication.

---
+++ src/main/java/uk/nhs/fhir/validator/ValidatorFacade.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.nhs.fhir.validator;
+
+import ca.uhn.fhir.model.dstu2.resource.BaseResource;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tim
+ */
+public class ValidatorFacade {
+    ArrayList<String> Validate(BaseResource resource, String profileURL) {
+        ArrayList<String> results = new ArrayList<String>();
+        
+        
+        return results;
+    }
+    
+}
