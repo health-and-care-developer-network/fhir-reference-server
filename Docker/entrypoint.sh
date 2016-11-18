cdc247821176ddb4a9144250b1e2b1b4a90117a1
Adam Hatherly <adam.hatherly@nhs.net>
2016-11-18 17:40:36 +0000

    Fix for publish docker entrypoint script

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -9,13 +9,13 @@ OLD_URL=$4
 NEW_URL=$5
 
 # First, clone the repository with our source files
-rm -Rf /source
-rm -Rf /generated
-git clone $GITHUB_URL /source
+rm -Rf /source/files
+git clone $GITHUB_URL /source/files
+cd /source/files
 git checkout $BRANCH
 
-sed -i -- "s|$OLD_URL|$NEW_URL|g" /source/StructureDefinitions/*
+sed -i -- "s|$OLD_URL|$NEW_URL|g" /source/files/StructureDefinitions/*
 
 cd /usr/makehtml
-java -cp ./target/MakeHTML-1.0-SNAPSHOT.jar uk.nhs.fhir.makehtml.NewMain /source/$REPO_PATH /generated
+java -cp ./target/MakeHTML-1.0-SNAPSHOT.jar uk.nhs.fhir.makehtml.NewMain /source/files/$REPO_PATH /generated
 
