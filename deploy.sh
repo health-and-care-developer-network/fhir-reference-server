f33aa3b76a82f1ec49f90016d4f2f21215112416
Adam Hatherly <adam.hatherly@nhs.net>
2016-11-21 21:00:44 +0000

    Tweak to deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,6 @@ CONTAINER_NAME=fhir-server
 
 REGISTRY_URL=$REGISTRY_HOST:5000
 
-POSTGRES_IMAGE="postgres:9.5"
-
 if [ -z $TARGET_HOST ]
 then
   TARGET_PREFIX=""
@@ -27,12 +25,17 @@ else
   REGISTRY_PREFIX="--tlsverify -H $REGISTRY_HOST:2376"
 fi
 
+MEMORYFLAG=1g
+CPUFLAG=768
+
 echo "Pull and run FHIR server"
 docker $TARGET_PREFIX pull $REGISTRY_URL/$IMAGE_NAME
 docker $TARGET_PREFIX stop $CONTAINER_NAME
 docker $TARGET_PREFIX rm $CONTAINER_NAME
-docker $TARGET_PREFIX run -p 8100:8080 --name $CONTAINER_NAME \
+docker $TARGET_PREFIX run -p 8080:8080 --name $CONTAINER_NAME \
 	--restart=on-failure:5 \
+        -m $MEMORYFLAG \
+	-c $CPUFLAG \
 	-v /docker-data/fhir-profiles:/opt/fhir
 	-v /docker-data/fhir-server-temp/jetty:/tmp/jetty
 	-d $REGISTRY_URL/$IMAGE_NAME
