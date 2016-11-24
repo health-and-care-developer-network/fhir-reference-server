8ef53409adf0dded67072b0faacc3cd82a67d3f0
Tim Coates <tim.coates@gmail.com>
2016-11-24 19:56:46 +0000

    Removed the HL7 Validator jar repo.

--- src/test/java/uk/nhs/fhir/util/FileLoaderTest.java
+++ src/test/java/uk/nhs/fhir/util/FileLoaderTest.java
@@ -21,29 +21,29 @@ import org.junit.Test;
 
 public class FileLoaderTest {
 
-	@Test
-	public void testRemoveFileExtension() {
-		
-		String test1 = "thisIs_a_filename.txt";
-		String test1e = "thisIs_a_filename";
-		
-		String test2 = "thisIs.a.filename.with.lots.of.dots.txt";
-		String test2e = "thisIs.a.filename.with.lots.of.dots";
-		
-		String test3 = "filename with spaces.txt";
-		String test3e = "filename with spaces";
-		
-		String test4 = "filename with spaces.and.dots.txt";
-		String test4e = "filename with spaces.and.dots";
-		
-		String test5 = "filenameWithNoExtension";
-		String test5e = "filenameWithNoExtension";
-		
-		assertEquals(FileLoader.removeFileExtension(test1), test1e);
-		assertEquals(FileLoader.removeFileExtension(test2), test2e);
-		assertEquals(FileLoader.removeFileExtension(test3), test3e);
-		assertEquals(FileLoader.removeFileExtension(test4), test4e);
-		assertEquals(FileLoader.removeFileExtension(test5), test5e);
-	}
+    @Test
+    public void testRemoveFileExtension() {
+
+        String test1 = "thisIs_a_filename.txt";
+        String test1e = "thisIs_a_filename";
+
+        String test2 = "thisIs.a.filename.with.lots.of.dots.txt";
+        String test2e = "thisIs.a.filename.with.lots.of.dots";
+
+        String test3 = "filename with spaces.txt";
+        String test3e = "filename with spaces";
+
+        String test4 = "filename with spaces.and.dots.txt";
+        String test4e = "filename with spaces.and.dots";
+
+        String test5 = "filenameWithNoExtension";
+        String test5e = "filenameWithNoExtension";
+
+        assertEquals(FileLoader.removeFileExtension(test1), test1e);
+        assertEquals(FileLoader.removeFileExtension(test2), test2e);
+        assertEquals(FileLoader.removeFileExtension(test3), test3e);
+        assertEquals(FileLoader.removeFileExtension(test4), test4e);
+        assertEquals(FileLoader.removeFileExtension(test5), test5e);
+    }
 
 }
