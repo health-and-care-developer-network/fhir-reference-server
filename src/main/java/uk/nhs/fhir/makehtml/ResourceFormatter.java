ff3f0e0b2ddae6d2f8c96b404da4575d7d7201fa
Kevin Mayfield <kevinmayfield@NJORD-3.local>
2017-04-18 09:04:01 +0100

    Moved to latest HAPI version and started work on ValueSet

--- src/main/java/uk/nhs/fhir/makehtml/ResourceFormatter.java
+++ src/main/java/uk/nhs/fhir/makehtml/ResourceFormatter.java
@@ -1,13 +1,15 @@
 package uk.nhs.fhir.makehtml;
 
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.hl7.fhir.instance.model.api.IBaseResource;
-
 import ca.uhn.fhir.model.dstu2.resource.OperationDefinition;
 import ca.uhn.fhir.model.dstu2.resource.StructureDefinition;
+import ca.uhn.fhir.model.dstu2.resource.ValueSet;
+import org.hl7.fhir.instance.model.api.IBaseResource;
 import uk.nhs.fhir.makehtml.opdef.OperationDefinitionFormatter;
 import uk.nhs.fhir.util.FhirDocLinkFactory;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+// KGM 13/Apr/2017 Added ValueSet
 
 public abstract class ResourceFormatter<T extends IBaseResource> {
 	public abstract HTMLDocSection makeSectionHTML(T source) throws ParserConfigurationException;
@@ -20,6 +22,8 @@ public abstract class ResourceFormatter<T extends IBaseResource> {
 			return (ResourceFormatter<T>) new OperationDefinitionFormatter();
 		} else if (resource instanceof StructureDefinition) {
 			return (ResourceFormatter<T>) new StructureDefinitionFormatter();
+		} else if (resource instanceof ValueSet) {
+			return (ResourceFormatter<T>) new ValueSetFormatter();
 		}
 
 		return null;
