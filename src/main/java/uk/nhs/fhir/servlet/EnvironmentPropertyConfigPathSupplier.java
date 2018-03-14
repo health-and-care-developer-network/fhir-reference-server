8c2eee62b4574dcb5380fddc3252b830999fa803
Adam Hatherly <adam.hatherly@nhs.net>
2018-03-14 18:02:37 +0000

    Fixes to passing in environment variable, and to use the new jar version

--- src/main/java/uk/nhs/fhir/servlet/EnvironmentPropertyConfigPathSupplier.java
+++ src/main/java/uk/nhs/fhir/servlet/EnvironmentPropertyConfigPathSupplier.java
@@ -3,6 +3,8 @@ package uk.nhs.fhir.servlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
+
 public class EnvironmentPropertyConfigPathSupplier implements ConfigPathSupplier {
 	private static final Logger LOG = LoggerFactory.getLogger(EnvironmentPropertyConfigPathSupplier.class);
 	
@@ -12,7 +14,7 @@ public class EnvironmentPropertyConfigPathSupplier implements ConfigPathSupplier
 	public String get() {
 		String envConfigFile = System.getenv("CONFIG_FILE");
 		
-		if (envConfigFile != null) {
+		if (!Strings.isNullOrEmpty(envConfigFile)) {
 			LOG.info("Using custom configuration from: " + envConfigFile);
 			return envConfigFile;
 		} else {
