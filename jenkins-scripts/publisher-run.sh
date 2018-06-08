30007c6fbfcf801ff9fc3970d0131f13f44f4e1d
Adam Hatherly <adam.hatherly@nhs.net>
2018-06-08 14:19:37 +0100

    Added github token passthrough into script

--- jenkins-scripts/publisher-run.sh
+++ jenkins-scripts/publisher-run.sh
@@ -15,6 +15,7 @@ TARGET_HOST="${TARGET_HOST:-${7}}"
 OUT_PATH="${OUT_PATH:-${8}}"
 TAG_NAME="${TAG_NAME:-${9}}"
 RENDERER_FLAGS="${RENDERER_FLAGS:-${10}}"
+GH_OAUTH="${GH_OAUTH:-${11}}"
 
 IMAGE_NAME="nhsd/fhir-profile-renderer"
 CONTAINER_NAME="fhir-publisher"
@@ -38,5 +39,6 @@ $TARGET_DOCKER_CMD run \
 	-v /docker-data/fhir-server-httpcache:/tmp/git-http-cache \
 	-v /docker-data/fhir-profiles:/generated \
 	-e "RENDERER_FLAGS=$RENDERER_FLAGS" \
+	-e "GH_OAUTH=$GH_OAUTH" \
 	"$SOURCE" "$GITHUB_URL" "$BRANCH" "$IN_PATH" "$OLD_URL" "$NEW_URL" "$OUT_PATH"
 
