afeafa1a6fce7bd65f7561e979ee02ae844ad7ae
akarimisetti <knvsanand@gmail.com>
2018-07-30 14:06:27 +0100

    ALP-327 fixes

--- reference-server-lib/src/main/java/uk/nhs/fhir/servlethelpers/RawResourceRenderer.java
+++ reference-server-lib/src/main/java/uk/nhs/fhir/servlethelpers/RawResourceRenderer.java
@@ -15,6 +15,28 @@ import uk.nhs.fhir.util.FhirContexts;
 import uk.nhs.fhir.util.FhirVersion;
 import uk.nhs.fhir.util.text.FhirTextSectionHelpers;
 
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import uk.nhs.fhir.servlet.SharedServletContext;
+
+import org.w3c.dom.Element;
+
 public class RawResourceRenderer {
 
     public String renderSingleWrappedRAWResourceWithoutText(IBaseResource resource, FhirVersion fhirVersion, String resourceName, ResourceType resourceType, String baseURL, MimeType mimeType) {
@@ -42,7 +64,12 @@ public class RawResourceRenderer {
         String serialised = fhirContext.newXmlParser().setPrettyPrint(true).encodeResourceToString(resource);
         
         // Encode it for HTML output
-        serialised = syntaxHighlight(serialised);
+        try {
+			serialised = syntaxHighlight(updateSoftwareNodes(serialised));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
         
         return serialised;
     }
@@ -51,4 +78,36 @@ public class RawResourceRenderer {
         // Serialise it to JSON
     	return fhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(resource);
     }
+    
+    public String updateSoftwareNodes(String xmltag ) throws Exception {
+        
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(xmltag)));
+        StreamResult result = new StreamResult(new StringWriter());
+      
+        // locate the node(s)
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        NodeList nodes = (NodeList)xpath.evaluate
+          ("//software/name", doc, XPathConstants.NODESET);
+      
+        // make the change
+        for (int idx = 0; idx < nodes.getLength(); idx++) {
+          ((Element) nodes.item(idx)).setAttribute("value", SharedServletContext.getProperties().getResourceSoftwareName());
+      
+        }
+           
+        nodes = (NodeList)xpath.evaluate
+              ("//software/version", doc, XPathConstants.NODESET);
+          
+        // make the change
+        for (int idx = 0; idx < nodes.getLength(); idx++) {
+              ((Element) nodes.item(idx)).setAttribute("value", SharedServletContext.getProperties().getResourceSoftwareVersion());            
+          }
+      
+              
+        // save the result
+        Transformer xformer = TransformerFactory.newInstance().newTransformer();
+        xformer.transform
+          (new DOMSource(doc), result);
+        return result.getWriter().toString();                       
+        }
 }
